a14f4cf02ca392fb6ea68e7f1945a2a64c61a32a
brito <jpbsmeira@gmail.com>
2025-12-08 16:28:21 -0300

    Atualização: adição do arquivo javascript para funcionamento da modal.

---
+++ 00_atividades/atividade_07/js/modal.js
@@ -0,0 +1,10 @@
+// Espera o DOM carregar
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Seleciona a modal
+    const minhaModal = new bootstrap.Modal(document.getElementById('minhaModalExemplo'));
+
+    // Abre a modal automaticamente
+    minhaModal.show();
+
+});
