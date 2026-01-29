1f6e9d4ca47e02ca5140472f90292c9a26f29a1f
britukk <jpbsmeira@gmail.com>
2026-01-29 19:55:53 -0300

    Início SpringBoot. Aula do dia 29/01/2025

---
+++ 05_spring/02_springfront/src/main/java/com/springfront/app/controllers/SpringFrontController.java
@@ -0,0 +1,15 @@
+package com.springfront.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpringFrontController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+}
